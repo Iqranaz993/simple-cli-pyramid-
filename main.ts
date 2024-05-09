2eb0a95b28d8316ff299ac12fa67f11b3337e7e2
Iqra naz <162154619+Iqranaz993@users.noreply.github.com>
2024-05-09 20:24:35 +0500

    This is simple cli base pyramid

---
+++ main.ts
@@ -0,0 +1,19 @@
+// import chalk from 'chalk';
+
+
+function Pyramid(height: number): void {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        
+        for (let j = 1; j <= height - i; j++) {
+            row += ' ';
+        }
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            row += '*';
+        }
+        console.log(row);
+    }
+}
+
+const pyramidHeight = 5;
+Pyramid(pyramidHeight);
